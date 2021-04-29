5d7def248b0627e3789d699fef4bcc3e1cf9e6c1
Cesar Cordoba <cordobacesar29@gmail.com>
2021-04-29 18:05:32 -0300

    Add Chakraprovider

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter as Router } from "react-router-dom";
-import './index.css';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>    
+    <ChakraProvider>
+      <Router>
+        <App />
+      </Router>  
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
