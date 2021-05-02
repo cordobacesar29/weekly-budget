1d47b615712b20848e5defaec60f108c16e48151
Cesar Cordoba <cordobacesar29@gmail.com>
2021-05-02 10:29:10 -0300

    create <LoginForm/> component

---
+++ src/component/LoginForm.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Flex } from '@chakra-ui/react';
+
+export const LoginForm = () => {
+    return ( 
+        <Flex 
+            margin='2rem'
+            padding='1rem'
+            w='15rem'
+            h='100vh'
+            border='1px solid #c2c2c2'
+            borderRaduis='8px'
+        >
+
+        </Flex>
+    )
+}
