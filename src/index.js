6d8ace6f7fc540dd170021bd5c3affffb9d3ba57
Cesar Cordoba <cordobacesar29@gmail.com>
2021-05-02 10:31:09 -0300

    add index.css

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from './App';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
