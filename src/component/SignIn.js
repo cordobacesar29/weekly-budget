3f1d4eed76d7ef46c85a62c7e68b72dd87bb025e
Cesar Cordoba <cordobacesar29@gmail.com>
2021-05-03 11:39:31 -0300

    create <SingIn/> component

---
+++ src/component/SignIn.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Flex, Text, Heading } from '@chakra-ui/react';
+import { InputForm } from './Input';
+import { ButtonForm } from './Button';
+
+export const SignIn = ( { toogleForm=()=>{} }) => {
+    return(
+        <Flex 
+                margin='2rem'
+                padding='2rem'
+                w='20rem'
+                border='1px solid #c2c2c2'
+                borderRadius='8px'
+                direction='column'
+            >
+                <Text mb='1rem'>Welcome to Weekly Budget</Text>
+                <Heading mb='1rem'>SignIn</Heading>
+                <InputForm
+                    className='email'
+                    placeholder='Email'
+                    value=''
+                    onChange=''
+                />
+                <InputForm
+                    className='password'
+                    placeholder='Password'
+                    value=''
+                    onChange=''
+                />
+                <ButtonForm 
+                    colorScheme='blue' 
+                    variant="solid"
+                >
+                    SignIn
+                </ButtonForm>
+                <ButtonForm  
+                    colorScheme="blue" 
+                    variant="ghost"
+                    toogleForm={toogleForm}
+                >
+                    First time here? SignUp!
+                </ButtonForm>
+            </Flex>
+    );
+}
