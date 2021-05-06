f4ca9837fbfe99432ed21b044c05de383190e6a3
César Córdoba <cordobacesar29@gmail.com>
2021-05-06 19:53:44 -0300

    <Footer/> component

---
+++ src/component/Footer.js
@@ -0,0 +1,9 @@
+import { Flex, Text } from '@chakra-ui/react';
+
+export const Footer = () => (
+    <Flex>
+        <Text
+            w='full' bg='gray.400' direction='row' px='10rem' py='2rem'
+        >Weekly Budget 2021</Text>
+    </Flex>
+)
