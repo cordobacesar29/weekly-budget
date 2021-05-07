9843e4ebd2d4d3708d0f1d4789d8697d00c5410d
César Córdoba <cordobacesar29@gmail.com>
2021-05-07 10:21:57 -0300

    useHistory & useLocalStorageState hooks for authenticated

--- src/component/SignIn.js
+++ src/component/SignIn.js
@@ -1,42 +1,76 @@
+import React from 'react';
+import {useHistory} from 'react-router-dom';
 import { Flex, Text, Heading, Input, Button } from '@chakra-ui/react';
+import {useLocalStorageState} from '../hooks/useLocalStorageState';
+import {sleep} from '../utils/sleep';
+
 
 export const SignIn = ( { toogleForm=()=>{} }) => {
+    const [ email, setEmail ] = React.useState('');
+    const [ password, setPassword ] = React.useState('');
+    
+    const [users] = useLocalStorageState('users', []);
+    const history = useHistory();
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const isAuthenticated = users.find((user) => {
+            return user.email === email && user.password === password;
+        });
+        console.log(isAuthenticated);
+        sleep().then(()=>{
+            history.push('/home'); 
+         });
+    };
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value);
+    };
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    };
     return(
         <Flex 
-                margin='2rem'
-                padding='2rem'
-                w='20rem'
-                border='1px solid #c2c2c2'
-                borderRadius='8px'
-                direction='column'
-            >
-                <Text mb='1rem'>Welcome to Weekly Budget</Text>
-                <Heading mb='1rem'>SignIn</Heading>
-                <Input
-                    mb='1rem'
-                    className='email'
-                    placeholder='Email'
-                />
-                <Input
-                    mb='1rem'
-                    className='password'
-                    placeholder='Password'
-                />
-                <Button
-                    mb='1rem'
-                    colorScheme='blue' 
-                    variant="solid"
-                >
-                    SignIn
-                </Button>
-                <Button  
-                    mb='1rem'
-                    colorScheme="blue" 
-                    variant="ghost"
-                    onClick={toogleForm}
-                >
-                    First time here? SignUp!
-                </Button>
-            </Flex>
+        as='form'
+        margin='2rem'
+        padding='2rem'
+        w='20rem'
+        border='1px solid #c2c2c2'
+        borderRadius='8px'
+        direction='column'
+        onSubmit={handleSubmit}
+    >
+        <Text mb='1rem'>Welcome to Weekly Budget</Text>
+        <Heading mb='1rem'>SignIn</Heading>
+        <Input
+            mb='1rem'
+            className='email'
+            placeholder='Email'
+            onChange={handleEmailChange}
+            
+        />
+        <Input
+            mb='1rem'
+            className='password'
+            placeholder='Password'
+            onChange={handlePasswordChange}
+        />
+        <Button
+            mb='1rem'
+            colorScheme='blue' 
+            variant="solid"
+            type='submit'
+        >
+            SignIn
+        </Button>
+        <Button  
+            mb='1rem'
+            colorScheme="blue" 
+            variant="ghost"
+            onClick={toogleForm}
+        >
+            First time here? SignUp!
+        </Button>
+        </Flex>
     );
 }
--- src/component/SignUp.js
+++ src/component/SignUp.js
@@ -1,15 +1,31 @@
 import React from 'react';
+import {useHistory} from 'react-router-dom';
 import { Flex, Heading, Text, Input, Button } from '@chakra-ui/react';
+import {useLocalStorageState} from '../hooks/useLocalStorageState';
+import {sleep} from '../utils/sleep';
 
 export const SignUp = ({ toogleForm=()=>{} }) => {
     const [ firstName, setFirstName ] = React.useState('');
     const [ lastName, setLastName ] = React.useState('');
     const [ email, setEmail ] = React.useState('');
     const [ password, setPassword ] = React.useState('');
+    const [users, setUsers] = useLocalStorageState('users', []);
+    const history = useHistory();
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        return(console.log('submit', firstName, lastName, email,password));
+
+        const user = {
+            firstName, 
+            lastName, 
+            email,
+            password,
+        };
+
+        setUsers([...users, user]);
+        sleep().then(()=>{
+           history.push('/signin'); 
+        });
     };
 
     const handleFirstNameChange = (e) => {
@@ -27,53 +43,53 @@ export const SignUp = ({ toogleForm=()=>{} }) => {
 
     return(
         <Flex 
-                as='form'
-                margin='2rem'
-                padding='2rem'
-                w='20rem'
-                border='1px solid #c2c2c2'
-                borderRadius='8px'
-                direction='column'
-                onSubmit={handleSubmit}
-            >   
-                <Text mb='1rem'>Welcome to Weekly Budget</Text>
-                <Heading mb='1rem'>SignUp</Heading>
-                <Input
-                    mb='1rem'
-                    placeholder='FirstName'
-                    onChange={handleFirstNameChange}
-                />
-                <Input
-                    mb='1rem'
-                    placeholder='LastName'
-                    onChange={handleLastNameChange}
-                />
-                <Input
-                    mb='1rem'
-                    placeholder='Email'
-                    onChange={handleEmailChange}
-                />
-                <Input
-                    mb='1rem'
-                    placeholder='Password'
-                    onChange={handlePasswordChange}
-                />
-                <Button 
-                    mb='1rem'
-                    colorScheme='blue' 
-                    variant="solid"
-                    type='submit'
-                >
-                    SignUp
-                </Button>
-                <Button  
-                    mb='1rem'
-                    colorScheme="blue" 
-                    variant="ghost"
-                    onClick={toogleForm}
-                >
-                    Already have an account? SignIn!
-                </Button>
-            </Flex>
+            as='form'
+            margin='2rem'
+            padding='2rem'
+            w='20rem'
+            border='1px solid #c2c2c2'
+            borderRadius='8px'
+            direction='column'
+            onSubmit={handleSubmit}
+        >   
+            <Text mb='1rem'>Welcome to Weekly Budget</Text>
+            <Heading mb='1rem'>SignUp</Heading>
+            <Input
+                mb='1rem'
+                placeholder='FirstName'
+                onChange={handleFirstNameChange}
+            />
+            <Input
+                mb='1rem'
+                placeholder='LastName'
+                onChange={handleLastNameChange}
+            />
+            <Input
+                mb='1rem'
+                placeholder='Email'
+                onChange={handleEmailChange}
+            />
+            <Input
+                mb='1rem'
+                placeholder='Password'
+                onChange={handlePasswordChange}
+            />
+            <Button 
+                mb='1rem'
+                colorScheme='blue' 
+                variant="solid"
+                type='submit'
+            >
+                SignUp
+            </Button>
+            <Button  
+                mb='1rem'
+                colorScheme="blue" 
+                variant="ghost"
+                onClick={toogleForm}
+            >
+                Already have an account? SignIn!
+            </Button>
+        </Flex>
     );
 }
