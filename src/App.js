e2108f2039f0c0080d073efedc6eff856f244705
César Córdoba <cordobacesar29@gmail.com>
2021-05-07 10:20:08 -0300

    import <Layout/> and add singIn - singUp route

--- src/App.js
+++ src/App.js
@@ -1,35 +1,44 @@
 import { Switch, Route } from "react-router-dom";
-import { LoginForm } from './component/LoginForm';
 import { Home } from './component/Home';
 import { Register } from './component/Register';
 import { Profile } from './component/Profile';
 import { Header } from './component/Header';
-
-
+import { SignIn } from './component/SignIn';
+import { SignUp } from './component/SignUp';
+import { Layout } from './component/Layout';
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
+    <Layout>
+      
       <Switch>
         <Route
-          exact path='/'
+          path='/signIn'
           component={()=>(
-            <LoginForm/>
+            <SignIn/>
           )}
         />
+        <Route
+          path='/signUp'
+          component={()=>(
+            <SignUp/>
+          )}
+        />
+        <Header/>
         <Route
           exact path='/home'
           component={()=>(
             <Home/>
           )}
         />
+        <Header/>
         <Route
           exact path='/register'
           component={()=>(
             <Register/>
           )}
         />
+        <Header/>
         <Route
           exact path='/profile'
           component={()=>(
@@ -37,7 +46,7 @@ function App() {
           )}
         />
       </Switch>
-    </div>
+    </Layout>
   );
 }
 
