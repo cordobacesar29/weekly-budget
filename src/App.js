446b44d5ca9b834459c6ed35693e89d2aa70710f
César Córdoba <cordobacesar29@gmail.com>
2021-05-08 14:56:23 -0300

    add route as children instaed props

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { Switch, Route } from "react-router-dom";
 import { Home } from './component/Home';
 import { Register } from './component/Register';
 import { Profile } from './component/Profile';
-import { Header } from './component/Header';
 import { SignIn } from './component/SignIn';
 import { SignUp } from './component/SignUp';
 import { Layout } from './component/Layout';
@@ -10,41 +9,22 @@ import { Layout } from './component/Layout';
 function App() {
   return (
     <Layout>
-      
       <Switch>
-        <Route
-          path='/signIn'
-          component={()=>(
-            <SignIn/>
-          )}
-        />
-        <Route
-          path='/signUp'
-          component={()=>(
-            <SignUp/>
-          )}
-        />
-        <Header/>
-        <Route
-          exact path='/home'
-          component={()=>(
-            <Home/>
-          )}
-        />
-        <Header/>
-        <Route
-          exact path='/register'
-          component={()=>(
-            <Register/>
-          )}
-        />
-        <Header/>
-        <Route
-          exact path='/profile'
-          component={()=>(
-            <Profile/>
-          )}
-        />
+        <Route exact path='/'> 
+          <Home/>
+        </Route>  
+        <Route path='/register'>
+          <Register/>
+        </Route> 
+        <Route path='/profile'>
+          <Profile/>
+        </Route>
+        <Route path='/signIn'>
+          <SignIn/>
+        </Route>
+        <Route path='/signUp'>
+          <SignUp/>
+        </Route>
       </Switch>
     </Layout>
   );
