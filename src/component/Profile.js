e0babe557684e887ffa54086a2617751d013fee2
César Córdoba <cordobacesar29@gmail.com>
2021-05-10 12:33:10 -0300

    signOut

--- src/component/Profile.js
+++ src/component/Profile.js
@@ -1,1 +1,26 @@
-export const Profile = () => (<h1>hola mundo profile</h1>)
+import React from 'react';
+import {Flex, Button} from '@chakra-ui/react';
+import {UserContext} from '../context/user';
+import {useHistory} from 'react-router-dom';
+ 
+export const Profile = () => {
+    const {setUser} = React.useContext(UserContext);
+    const history = useHistory();
+
+    const handleSignOut = () =>{
+        setUser(null);
+
+        history.push('/signup');
+    };
+    return(
+        <Flex>
+            <h1>Hello Profile</h1>
+            <Button
+                mb='1rem'
+                colorScheme='blue' 
+                variant="solid"
+                onClick={handleSignOut}
+            >Sign Out</Button>
+        </Flex>
+    )
+}
